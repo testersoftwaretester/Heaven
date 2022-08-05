ca2bcc5ccbd9ab2abb0e7ded16d5631c46aeff42
testersoftwaretester <109034320+testersoftwaretester@users.noreply.github.com>
2022-08-05 21:33:11 +0530

    This description

---
+++ DEV/NewArch/new/mains.tf
@@ -0,0 +1,17 @@
+
+resource"azurerm_function_app_slot""Function_application_slot8967410000"  { 
+
+
+}
+
+ 
+
+ 
+
+ 
+resource"azurerm_function_app_slot""Function_application_slot1306710000"  { 
+
+
+app_service_plan_id="pokjhgvc" 
+}
+
